185244b07f64cfba124b700a933f5ef5278d00f3
Marcello <64552456+The-Crow-pleb@users.noreply.github.com>
2020-06-15 10:19:06 -0300

    Comando atualizado!

--- src/commands/mod/unban.js
+++ src/commands/mod/unban.js
@@ -1,18 +1,32 @@
 module.exports = {
     run: async(client, message, args) => {
+        const { guild } = message
+        const { MessageEmbed } =require('discord.js')
         if(!message.member.hasPermission('BAN_MEMBERS')) {
-                message.channel.send("Você não tem acessso ao comando!!")
+            message.channel.send("Você não tem acessso ao comando!!")
         }
         else {
             let memberId = message.content.substring(message.content.indexOf(' ') + 1)
             let member = message.guild.members.cache.get(memberId);
             try {
                 let bannedMember = await message.guild.members.unban(memberId);
-                if(bannedMember)
-                    message.reply(bannedMember.tag + " Foi desbanido, nojo, você teve piedade. . .");
-            }
-            catch(err) {
-                message.reply(`Não consegui desbanir o membro!\nEu só consigo desbanir via o ID, tente isso!\n||Você tem certeza que quer ter piedade de alguém?||`)
+                if(bannedMember) {
+                    const embed = new MessageEmbed()
+                    .setTitle('Ação: Desbanimento.')
+                    .setDescription(`${bannedMember.tag} foi desbanido(a) com sucesso!`)
+                    .setColor('RANDOM')
+                    .setAuthor(`${guild.name}`, guild.iconURL({ dynamic: true }))
+                    .setFooter(`Desbanimento realizado por: ${message.author.tag}`);
+                    message.channel.send(embed)
+                }
+        }
+        catch(err) {
+            const embed = new MessageEmbed()
+            .setTitle('Falha!')
+            .setDescription(`Não foi possível desbanir o membro com o id ${memberId}, tenha certeza de utilizar o ID correto!`)
+            .setColor('RANDOM')
+            .setAuthor(`${guild.name}`, guild.iconURL({ dynamic: true }))
+            message.channel.send(embed)
             }
         }
     },
