c1503b17784e3119dc315aa7b7b307333879d685
Marcello <64552456+The-Crow-pleb@users.noreply.github.com>
2020-06-15 10:17:36 -0300

    Comando atualizado

--- src/commands/mod/kick.js
+++ src/commands/mod/kick.js
@@ -2,30 +2,33 @@ const discord = require("discord.js");
 
 module.exports = {
     run: (client, message, args) => {     
+      const { guild } = message
+      const { discord } = require("discord.js");
+      const { MessageEmbed } = require('discord.js')
+      const user = message.mentions.users.first();
+
       if(!message.member.hasPermission("KICK_MEMBERS")) {
-        return message.channel.send(`**${message.author.username}**, Você não tem permissão para isso!`)
+          return message.channel.send(`**${message.author.username}**, Você não tem permissão para isso!`)
       }     
       if(!message.guild.me.hasPermission("KICK_MEMBERS")) {
-        return message.channel.send(`**${message.author.username}**, Eu não tenho permissão para fazer isso!`)
+          return message.channel.send(`**${message.author.username}**, Eu não tenho permissão para fazer isso!`)
       }      
       let target = message.mentions.members.first();     
       if(!target) {
-        return message.channel.send(`**${message.author.username}**, Por favor, mencione alguém.`)
+          return message.channel.send(`**${message.author.username}**, Por favor, mencione alguém.`)
       }
       if(target.id === message.author.id) {
-       return message.channel.send(`**${message.author.username}**, Não é mais fácil usar o botão de "Sair do Servidor" nesse caso?`)
+          return message.channel.send(`**${message.author.username}**, Não é mais fácil usar o botão de "Sair do Servidor" nesse caso?`)
       }
-    if(!args[1]) {
-      return message.channel.send(`**${message.author.username}**, Me dê um motivo para cumprir a ordem!`)
-    }
-      let embed = new discord.MessageEmbed()
-      .setTitle("Ação: Expulsão")
-      .setDescription(`${target} foi expulso(a) do servidor!`)
-      .setColor("#ff2050")
-      .setThumbnail('https://cdn.discordapp.com/attachments/694416599939022848/710235085957365810/DoctorDance.gif')
-      .setFooter(`${message.author.username} expulsou ${target}`);
-      message.channel.send(embed)
-      target.kick(args[1]);
+        const embed = new MessageEmbed()
+          .setTitle("Ação: Expulsão")
+          .setAuthor(`${guild.name}`, guild.iconURL({ dynamic: true }))
+          .setDescription(`${target} foi expulso(a) do servidor!`)
+          .setColor("#ff2050")
+          .setImage('https://i.pinimg.com/originals/6f/f1/40/6ff14029eb25bbbe796bcec5112eff67.gif')
+          .setFooter(`${message.author.username} expulsou ${target}`);
+              message.channel.send(embed)
+              target.kick(args[1]);
     },
     aliases: ['kc'],
     description: 'Retira um membro do servidor!'
