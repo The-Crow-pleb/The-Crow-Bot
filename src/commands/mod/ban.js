86a4179c0e976c5dfc686d87f6dd243c9a597b11
Marcello <64552456+The-Crow-pleb@users.noreply.github.com>
2020-06-15 10:07:32 -0300

    Comandos ban e idban mesclados e refeitos

--- src/commands/mod/ban.js
+++ src/commands/mod/ban.js
@@ -2,32 +2,48 @@ const discord = require("discord.js");
 
 module.exports = {
     run: async (client, message, args) => {
+      const { guild } = message
+      const { discord } = require("discord.js");
+      const { MessageEmbed } = require('discord.js')
+      const user = message.mentions.users.first();
+
       if(!message.member.hasPermission("BAN_MEMBERS")) {
-        return message.channel.send(`**${message.author.username}**, Você não tem permissão para banir alguém!`)
+          return message.channel.send(`**${message.author.username}**, Você não tem permissão para banir alguém!`)
       }
       if(!message.guild.me.hasPermission("BAN_MEMBERS")) {
-        return message.channel.send(`**${message.author.username}**, Eu não tenho permissão para banir!`)
+          return message.channel.send(`**${message.author.username}**, Eu não tenho permissão para banir!`)
       }
-      const target = message.mentions.members.first();
-      if(!target) {
-        return message.channel.send(`**${message.author.username}**, Por favor, mencione quem deseja banir!`)
+      if (!user) {
+          try {
+              const { MessageEmbed } = require('discord.js')
+              let memberId = message.content.substring(message.content.indexOf(' ') + 1)
+              let member = message.guild.members.cache.get(memberId);
+              let bannedMember = await message.guild.members.ban(memberId)
+                  if(bannedMember) {
+                      const embed = new MessageEmbed()
+                      .setTitle('Ação: Banimento.')
+                      .setDescription(`${bannedMember.tag} foi banido(a) com sucesso!`)
+                      .setColor('RANDOM')
+                      .setAuthor(`${guild.name}`, guild.iconURL({ dynamic: true }))
+                      .setFooter(`Banimento realizado por: ${message.author.tag}`);
+                      message.channel.send(embed)
+                  }
+          } catch (err) {
+              message.reply('não')
+          }
+          
+      } else {
+          await message.guild.members.ban(user)
+          const embed = new MessageEmbed()
+            .setTitle('Ação: Banimento.')
+            .setDescription(`${user.tag} foi banido(a) com sucesso!`)
+            .setColor('RANDOM')
+            .setAuthor(`${guild.name}`, guild.iconURL({ dynamic: true }))
+            .setFooter(`Banimento realizado por: ${message.author.tag}`);
+              message.channel.send(embed)
+              await message.guild.members.ban(user)
       }
-      if(target.id === message.author.id) {
-        return message.channel.send(`**${message.author.username}**, Você não pode se banir -.-`)
-      }
-     if(!args[1]) {
-       return message.channel.send(`**${message.author.username}**, Por favor, mencione uma razão para o banimento, se possivel`)
-     }
-      let embed = new discord.MessageEmbed()
-      .setTitle("Ação: Ban")
-      .setDescription(`${target} foi banido(a).`)
-      .setColor("RANDOM")
-      .setThumbnail('https://cdn.discordapp.com/attachments/694416599939022848/710235085957365810/DoctorDance.gif')
-      .setFooter(`Banimento realizado por: ${message.author.tag}`);
-      
-      message.channel.send(embed)
-      target.ban(args[1])
     },
-    aliases: ['userban'],
-    description: 'Bane um membro'
+    aliases: [],
+    description: ''
   }
