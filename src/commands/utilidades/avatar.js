128c5c8121c75effb234d8dbe226a08c91e17b5f
Marcello <64552456+The-Crow-pleb@users.noreply.github.com>
2020-06-16 08:28:26 -0300

    Comando 'modernizado'

--- src/commands/utilidades/avatar.js
+++ src/commands/utilidades/avatar.js
@@ -2,28 +2,26 @@ module.exports = {
     run: async(client, message) => {
         const { MessageEmbed } = require('discord.js')
         if(message.author.bot) return;
-        if(message.content.toLowerCase().startsWith('c!avatar')) {
-          const args = message.content.split(' ');
-          if(args.length > 2) {
-            message.channel.send(`Como usar: c!avatar <user_id> | c!avatar @mention`);
-        } else if(args.length === 2) {
-            const member = message.mentions.members.size === 1 ? 
-              message.mentions.members.first() :
-              message.guild.members.cache.get(args[1]);
-            if(member) {
-                const { guild } = message;
-                const embed = new MessageEmbed()
-                .setAuthor(`${guild.name}`, guild.iconURL({ dynamic: true }))
-                .setDescription('Esta é a imagem que o usuário utiliza no momento!:')
-                .setThumbnail(member.user.avatarURL({ dynamic: true }))
-                .addField('Este é o link do avatar:  ', member.user.avatarURL({ dynamic: true }))
-                .setColor('#01d410')
-                message.channel.send(embed);
-            } else {
-              message.channel.send(`Eu não consegui encontrar o usuario com o ID ou Menção: ${args[1]}, será que ele aprendeu a voar?`);
-            }
-        }
-        }
+        const args = message.content.split(' ');
+        if(args.length > 2) {
+          message.channel.send(`Como usar: c!avatar <user_id> | c!avatar @mention`);
+      } else if(args.length === 2) {
+          const member = message.mentions.members.size === 1 ? 
+            message.mentions.members.first() :
+            message.guild.members.cache.get(args[1]);
+          if(member) {
+              const { guild } = message;
+              const embed = new MessageEmbed()
+              .setTitle(`🔍${message.author.username}`)
+              .setDescription(`[Esta](${member.user.avatarURL({format: 'png', dynamic: true, size:2048})}) é a imagem que o usuário utiliza no momento`)
+              .setImage(member.user.avatarURL({format: 'png', dynamic: true, size: 2048 }))
+              .setColor('RANDOM')
+              message.channel.send(embed);
+          } else {
+            message.channel.send(`Eu não consegui encontrar o usuario com o ID ou Menção: ${args[1]}, será que ele aprendeu a voar?`);
+          }
+
+      }
     },
     aliases: [""],
     description: "Mostra o Avatar do usuario!"
