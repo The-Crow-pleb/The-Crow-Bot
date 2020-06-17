c3d6da1a11a3f630627e802b0617e3a7ff7074a1
Marcello <64552456+The-Crow-pleb@users.noreply.github.com>
2020-06-17 12:58:44 -0300

    Comando atualizado

--- src/commands/utilidades/help.js
+++ src/commands/utilidades/help.js
@@ -15,7 +15,7 @@ module.exports = {
             .setAuthor(`${guild.name}`, guild.iconURL({ dynamic: true }))
             .setThumbnail(guild.iconURL({ dynamic: true }))
             .addField("Meu prefixo é:", "c!")
-            .addField("Utilidades: ", "c!help (Bem... Eu acho que não preciso explicar muito.)\n\nc!avatar (Mostra o avatar da pessoa mencionada ou pelo ID)\n\nc!info (Mostra informações do usuario **presente no servidor** mencionado/pelo ID do mesmo, ou, informações **deste** servidor)\n\nc!ping (Mostra o meu ping e o ping que eu tenho da API do Discord!)")
+            .addField("Utilidades: ", "c!convite(Para me convidar a um outro servidor!)\n\nc!help (Bem... Eu acho que não preciso explicar muito.)\n\nc!avatar (Mostra o avatar da pessoa mencionada ou pelo ID)\n\nc!info (Mostra informações do usuario **presente no servidor** mencionado/pelo ID do mesmo, ou, informações **deste** servidor)\n\nc!ping (Mostra o meu ping e o ping que eu tenho da API do Discord!)")
             .addField("Moderação: ", "c!ban (Bane um membro do servidor)\n\nc!kick (Expulsa um membro do servidor)\n\nc!mute (Muta um membro)\n\nc!unban (Bem, todo comando de desbanimento só funciana por ID, então...)\n\nc!unmute (Desmuta um membro)")
             .addField("Diversões: ", "c!dado (Roda um dado de número aleatório entre 1 e 25!)")
             .setFooter("Sim, eu sei, não tenho muitas funcionalidades, mas eu ainda estou em desenvolvimento, irei logo mais ser aprimorado e ser um Corvo melhor!\n\nMinha versão atual é: 1.5.0")
