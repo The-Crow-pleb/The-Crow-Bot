d163167815071bd6ba65422ac104f87a899298d6
Marcello <64552456+The-Crow-pleb@users.noreply.github.com>
2020-06-18 13:44:36 -0300

    Update toska.js

--- src/commands/não sei porra/toska.js
+++ src/commands/não sei porra/toska.js
@@ -12,7 +12,7 @@ module.exports = {
             .setTitle('Um dia, tudo voltará.')
             .setAuthor(`${guild.name}`, guild.iconURL({ dynamic: true }))
             .setImage("https://media3.giphy.com/media/mHqF3NgGhyFJS/source.gif")
-            .addField("Corvos costumam voar para longe, sabia?", 'Quando um corvo tem sua asa ferida, ele é obrigado a repousar e ficar no mesmo lugar, as vezes, infelizmente, alguns humanos cuidam desse corvo, e corvos são como cães, eles se apegam aos humanos, e quando um corvo se apega a um, ele não quer deixar aquele lugar.\nCorvos são livres, um dia, por mais apegado que seja, ele voará e não voltará. Corvos também sentem ódio, sentem o fervor dele por suas penas, corvos são tão humanos quanto você.\nQuer saber o que significa Toska? Clique [aqui](https://www.urbandictionary.com/define.php?term=Toska) e você verá.\n||5949, existem muitas surpresas por aí, não acha?||')
+            .addField("Corvos costumam voar para longe, sabia?", 'Quando um corvo tem sua asa ferida, ele é obrigado a repousar e ficar no mesmo lugar, as vezes, infelizmente, alguns humanos cuidam desse corvo, e corvos são como cães, eles se apegam aos humanos, e quando um corvo se apega a um, ele não quer deixar aquele lugar.\nCorvos são livres, um dia, por mais apegado que seja, ele voará e não voltará. Corvos também sentem ódio, sentem o fervor dele por suas penas, corvos são tão humanos quanto você.\nQuer saber o que significa Toska? Clique [aqui](https://www.urbandictionary.com/define.php?term=Toska) e você verá.\n5949, existem muitas surpresas por aí, não acha?')
             .setColor('RANDOM')
             message.channel.send(embed);
         }
