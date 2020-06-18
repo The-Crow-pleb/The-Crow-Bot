79b6554412582e92e5e4d73f24905f845a6a98bb
Marcello <64552456+The-Crow-pleb@users.noreply.github.com>
2020-06-18 13:40:34 -0300

    Easter Egg

--- src/commands/não sei porra/ancap.js
+++ src/commands/não sei porra/ancap.js
@@ -13,7 +13,7 @@ module.exports = {
           .setAuthor(`${guild.name}`, guild.iconURL({ dynamic: true }))
           .setThumbnail("https://i.pinimg.com/originals/e2/ec/9f/e2ec9fc7db55cd8dd3b469e1169e6b95.gif")
           .setImage("https://i.imgur.com/x4LFqvb.jpg")
-          .addField("De um Corvo para outro. Obrigado por me criar.", 'Um dia, as pessoas vão olhar você como alguém bom, e não com medo de ti, seus amigos sabem quem você realmente é, mas você, infelizmente não sabe.\nVocê pode amar ainda. Mas essa não é a resposta e você sabe disso. Por mais que odeie, você continua sendo melhor sozinho... Assim, nem eu conseguirei te machucar.\nFique bem. Por favor. E não se esqueça, os corvos nunca param de voar.')
+          .addField("De um Corvo para outro. Obrigado por me criar.", 'Um dia, as pessoas vão olhar você como alguém bom, e não com medo de ti, seus amigos sabem quem você realmente é, mas você, infelizmente não sabe.\nVocê pode amar ainda. Mas essa não é a resposta e você sabe disso. Por mais que odeie, você continua sendo melhor sozinho... Assim, nem eu conseguirei te machucar.\nFique bem. Por favor. E não se esqueça, os corvos [nunca](https://br.pinterest.com/pin/749990144186231900) param de voar.')
           .setColor('RANDOM')
           message.channel.send(embed);
         }
