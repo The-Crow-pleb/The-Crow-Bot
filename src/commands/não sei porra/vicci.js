955d8a9436d6a50d8114434c9c8da0d5f74f709d
Marcello <64552456+The-Crow-pleb@users.noreply.github.com>
2020-07-12 11:24:57 -0300

    Der Krähe und Der Wolf

--- src/commands/não sei porra/vicci.js
+++ src/commands/não sei porra/vicci.js
@@ -9,10 +9,10 @@ module.exports = {
             message.channel.send(`Tente novamente!`);
         } else if(args.length === 1) {
             const embed = new MessageEmbed()
-            .setTitle('Vicci, o "Mono" "Talon"')
+            .setTitle('Die Krähe wird dich nie vergessen. Diesmal brauchte der Wolf die Hilfe der Krähe.')
             .setAuthor(`${guild.name}`, guild.iconURL({ dynamic: true }))
-            .setImage("https://i.pinimg.com/originals/ed/51/a3/ed51a3234379d2555a1a904445b834a2.gif")
-            .addField("De um corvo, para.... O quê você é?", 'Se algum dia você me encontrar voando por aí, apenas me observe e aprecie o meu vôo, mas por favor, não tente impedir minhas asas de bater.\nUm dia, eu voarei mais alto que todos, eu prometo.')
+            .setImage("https://i.pinimg.com/564x/a1/b7/8e/a1b78e0f3f85feed610491195c6e625c.jpg")
+            .addField("Von einer Krähe zu einem Wolf", 'Eu disse, você me veria voar novamente, com as asas tão fortes quanto antes, com sua vitalidade restaurada.\nVocê pode não se considerar um, mas eu te considero, after all, the Crow need the help from the Wolf to get his preys killed, isnt it? Now, the Crow will help you. Bleed for me, I will bleed for you.')
             .setColor('RANDOM')
             message.channel.send(embed);
         }
