9a7b07805ca3738e7b7e8000be7a9debbbea1e0a
Marcello <64552456+The-Crow-pleb@users.noreply.github.com>
2020-07-12 11:32:03 -0300

    Mudança para o Nicolas

--- src/commands/não sei porra/luiza.js
+++ src/commands/não sei porra/luiza.js
@@ -3,16 +3,16 @@ module.exports = {
         const { MessageEmbed } = require('discord.js')
         const { guild } = message;
         if(message.author.bot) return;
-        if(message.content.toLowerCase().startsWith('c!luiza')) {
+        if(message.content.toLowerCase().startsWith('c!nico')) {
           const args = message.content.split(' ');
           if(args.length > 1) {
             message.channel.send(`Tente Novamente!`);
         } else if(args.length === 1) {
             const embed = new MessageEmbed()
-            .setTitle('Luluzinha OwO')
+            .setTitle('Hey-o Nicoras')
             .setAuthor(`${guild.name}`, guild.iconURL({ dynamic: true }))
-            .setImage("https://i.pinimg.com/originals/10/1d/6d/101d6d064d43691fd929ac37cc1a6b74.gif")
-            .addField("Lolizinha fofa", 'Sai da moto, interesseira. E não esquece, antes de sair, manda foto da coxa pls')
+            .setImage("https://i.pinimg.com/originals/01/80/61/018061c4115995fa9a896cfb107a512a.gif")
+            .addField("PRET FEDID", 'Saca, você foi uma das primeiras pessoas que meu criador conheceu por aqui, sua amizade com ele durou por esse ano (Sim, ANO) e provavelmente chegue a durar mais, você é valioso para ele, sabia? Apesar de toda a merda, ele nunca sairá de perto, tenha certeza disso, conte com a minha e a ajuda dele!')
             .setColor('RANDOM')
             message.channel.send(embed);
         }
